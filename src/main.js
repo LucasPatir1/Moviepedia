40b1742b8a862caeecf2026a5e8f3f130afcc7b1
Diego Tellez <diegotellezcontreras@gmail.com>
2022-12-08 22:47:38 -0500

    Avances en navegación y estilos

--- src/main.js
+++ src/main.js
@@ -8,15 +8,18 @@ const api = axios.create({
     },
 })
 
-async function getTrendingMoviesPreview() {
-    const { data } = await api("trending/movie/day")
-    const movies = data.results
+// Utils
+
+function createMovies(movies, container) {
+    container.innerHTML = "";
 
     movies.forEach(movie => {
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
-        
+
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
+        movieContainer.addEventListener("click", () => {
+            location.hash = "#movie=" + movie.id
+        })
     
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
@@ -27,16 +30,14 @@ async function getTrendingMoviesPreview() {
         );
     
         movieContainer.appendChild(movieImg);
-        trendingPreviewMoviesContainer.appendChild(movieContainer);
+        container.appendChild(movieContainer);
     });
 }
 
-async function getCategoriesPreview() {
-    const { data } = await api("genre/movie/list")
-    const categories = data.genres
+function createCategories(categories, container) {
+    container.innerHTML = "";
 
     categories.forEach(category => {
-        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
         
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
@@ -44,11 +45,91 @@ async function getCategoriesPreview() {
         const categoryTitle = document.createElement('h3');
         categoryTitle.classList.add('category-title');
         categoryTitle.setAttribute('id', "id" + category.id);
+        categoryTitle.addEventListener("click", () => {
+            location.hash = `#category=${category.id}-${category.name}`
+        })
         const categoryTitleText = document.createTextNode(category.name)
         
         categoryTitle.appendChild(categoryTitleText)
         categoryContainer.appendChild(categoryTitle)
-        previewCategoriesContainer.appendChild(categoryContainer)
+        container.appendChild(categoryContainer)
     });
 }
 
+
+// Llamados a la API
+async function getTrendingMoviesPreview() {
+    const { data } = await api("trending/movie/day")
+    const movies = data.results
+
+    createMovies(movies, trendingMoviesPreviewList)
+}
+
+async function getCategoriesPreview() {
+    const { data } = await api("genre/movie/list")
+    const categories = data.genres
+
+    categoriesPreviewList.innerHTML = ""
+    createCategories(categories, categoriesPreviewList)
+}
+
+async function getMoviesByCategory(id) {
+    const { data } = await api("discover/movie", {
+        params: {
+            with_genres: id,
+        }
+    })
+    const movies = data.results
+    
+    createMovies(movies,genericSection);
+}
+
+async function getMoviesBySearch(query) {
+    console.log(query);
+    const { data } = await api("search/movie", {
+        params: {
+            query,
+        }
+    })
+    const movies = data.results
+    
+    createMovies(movies,genericSection);
+    
+}
+
+async function getTrendingMovies() {
+    const { data } = await api("trending/movie/day")
+    const movies = data.results
+
+    createMovies(movies, genericSection)
+}
+
+async function getMovieById(id) {
+    const { data: movie } = await api("movie/" + id)
+
+    const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+
+    headerSection.style.background = `
+        linear-gradient(
+            180deg, 
+            rgba(0, 0, 0, 0.35) 19.27%, 
+            rgba(0, 0, 0, 0) 29.17%
+        ),
+        url(${movieImgUrl})`;
+
+    movieDetailTitle.textContent = movie.title
+    movieDetailDescription.textContent = movie.overview
+    movieDetailScore.textContent = movie.vote_average
+
+    createCategories(movie.genres, movieDetailCategoriesList)
+    
+    getRelatedMoviesId(id)
+}
+
+async function getRelatedMoviesId(id) {
+    const { data } = await api(`movie/${id}/similar`)
+    const relatedMovies = data.results
+
+    createMovies(relatedMovies, relatedMoviesContainer)
+    relatedMoviesContainer.scrollTo(0, 0);
+}
--- src/navigation.js
+++ src/navigation.js
@@ -1,3 +1,24 @@
+searchFormBtn.addEventListener("click", () => {
+    location.hash = "#search=" + searchFormInput.value    
+})
+
+trendingBtn.addEventListener("click", () => {
+    location.hash = "#trends"
+})
+
+arrowBtn.addEventListener("click", () => {
+    history.back();
+    // location.hash = "#home"
+})
+
+searchFormInput.addEventListener('keyup', (event) => {
+    if(event.key === 'Enter') {
+        const input = searchFormInput.value.trim();
+        location.hash = `#search=${searchFormInput.value.trim()}`;
+    }
+});
+
+
 window.addEventListener("DOMContentLoaded", navigator, false)
 window.addEventListener("hashchange", navigator, false)
 
@@ -15,27 +36,117 @@ function navigator () {
     } else {
         homePage()
     }
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function homePage() {
     console.log("HOME!!");
+
+    
+    headerSection.classList.remove("header-container--long")
+    headerSection.style.background = "";
+    arrowBtn.classList.add("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.remove("inactive")
+    headerCategoryTitle.classList.add("inactive")
+    searchForm.classList.remove("inactive")
+
+    trendingPreviewSection.classList.remove("inactive")
+    categoriesPreviewSection.classList.remove("inactive")
+    genericSection.classList.add("inactive")
+    movieDetailSection.classList.add("inactive")
+
     getTrendingMoviesPreview()
     getCategoriesPreview()
 }
 
-function trendsPage() {
-    console.log("TRENDS!!");
-}
+function categoriesPage() {
+    console.log("CATEGORIES!!");
 
-function searchPage() {
-    console.log("SEARCH!!");
+    headerSection.classList.remove("header-container--long")
+    headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.remove("inactive")
+    searchForm.classList.add("inactive")
+
+    trendingPreviewSection.classList.add("inactive")
+    categoriesPreviewSection.classList.add("inactive")
+    genericSection.classList.remove("inactive")
+    movieDetailSection.classList.add("inactive")
+
+    const [_, categoryData] = location.hash.split("=")
+    const [categoryId, categoryName] = categoryData.split("-")
+    const categoryNameOk = categoryName.replace("%20", " ")
+
+    getMoviesByCategory(categoryId);
+    headerCategoryTitle.innerHTML = categoryNameOk
 }
 
 function movieDetailsPage() {
     console.log("MOVIE!!");
+
+    headerSection.classList.add("header-container--long")
+    // headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.add("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.add("inactive")
+    searchForm.classList.add("inactive")
+
+    trendingPreviewSection.classList.add("inactive")
+    categoriesPreviewSection.classList.add("inactive")
+    genericSection.classList.add("inactive")
+    movieDetailSection.classList.remove("inactive")
+
+    const [_, movieId] = location.hash.split("=")
+    getMovieById(movieId);
 }
 
-function categoriesPage() {
-    console.log("CATEGORIES!!");
+function searchPage() {
+    console.log("SEARCH!!");
+
+    headerSection.classList.remove("header-container--long")
+    headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.add("inactive")
+    searchForm.classList.remove("inactive")
+
+    trendingPreviewSection.classList.add("inactive")
+    categoriesPreviewSection.classList.add("inactive")
+    genericSection.classList.remove("inactive")
+    movieDetailSection.classList.add("inactive")
+
+    const [_, query] = location.hash.split("=");
+    // const query = searchFormInput.value
+    getMoviesBySearch(query)
 }
+
+function trendsPage() {
+    console.log("TRENDS!!");
+
+    headerSection.classList.remove("header-container--long")
+    headerSection.style.background = "";
+    arrowBtn.classList.remove("inactive");
+    arrowBtn.classList.remove("header-arrow--white");
+    headerTitle.classList.add("inactive")
+    headerCategoryTitle.classList.remove("inactive")
+    searchForm.classList.add("inactive")
+
+    trendingPreviewSection.classList.add("inactive")
+    categoriesPreviewSection.classList.add("inactive")
+    genericSection.classList.remove("inactive")
+    movieDetailSection.classList.add("inactive")
+
+    headerCategoryTitle.innerHTML = "Trendings"
+    getTrendingMovies()
+}
+
+
+
 
